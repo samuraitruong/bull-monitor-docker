c194652971d7c4e0f4b8aff6e4afc9fbb8acb078
Truong Nguyen <tnguyen@demystdata.com>
2021-06-05 13:15:20 +1000

    add healthcheck for koa provider

--- src/koa.js
+++ src/koa.js
@@ -1,11 +1,16 @@
 import Koa from 'koa';
 import Queue from 'bull';
 import pkg from '@bull-monitor/koa';
+import Router from '@koa/router';
 const { BullMonitorKoa } = pkg;
 
 export default async function withKoa(redisUrl, queueNames, port) {
   const queues = queueNames.map((queueName) => new Queue(queueName, redisUrl));
   const app = new Koa();
+  const router = new Router();
+  router.get('/ping', (ctx, next) => {
+    ctx.body = 'OK';
+  });
   const monitor = new BullMonitorKoa({
     queues,
     baseUrl: '/',
@@ -16,10 +21,10 @@ export default async function withKoa(redisUrl, queueNames, port) {
   await monitor.init({
     // optional middleware that will run before the bull-monitor router
     middleware: async (_ctx, next) => {
-      console.log('do something');
       await next();
     },
   });
+  app.use(router.routes());
   app.use(monitor.router.routes());
 
   app.listen(port, () => {
